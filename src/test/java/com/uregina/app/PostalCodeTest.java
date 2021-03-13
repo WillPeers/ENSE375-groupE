138fe7186d7cc71b409ce6b878495d187d80c88e
alishkad7865 <alishkadiwal@uregina.ca>
2021-03-12 19:40:00 -0700

    test v 1.2

--- src/test/java/com/uregina/app/PostalCodeTest.java
+++ src/test/java/com/uregina/app/PostalCodeTest.java
@@ -1,6 +1,7 @@
 package com.uregina.app;
 
-import static org.junit.Assert.assertTrue;
+import com.uregina.exception.*;
+import static org.junit.Assert.*;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertEquals;
 
@@ -11,82 +12,121 @@ import jdk.jfr.Timestamp;
 /**
  * Unit test for simple PostalCode.
  */
-public class PostalCodeTest 
-{
+public class PostalCodeTest {
     /**
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
     }
 
     @Test
     public void ValidPostalCodeTrue() {
-        PostalCode code = new PostalCode("K1B-4J2");
-        assertTrue(isValidPostalCode(code));
+        try {
+            PostalCode code = new PostalCode("K1B-4J2");
+            assertTrue(code.isValidPostalCode(code.getPostalCode()));
+        } catch (InvalidPostalCodeException e) {
+            //
+        }
+
     }
 
     @Test
     public void InvalidPostalCode_BadPrefix() {
-        PostalCode code = new PostalCode("K2B-4J2");
-        assertFalse(isValidPostalCode(code));
+        try {
+            PostalCode code = new PostalCode("K2B-4J2");
+            assertFalse(code.isValidPostalCode(code.getPostalCode()));
+        } catch (InvalidPostalCodeException e) {
+            //
+            //
+        }
     }
 
     @Test
     public void InvalidPostalCode_VindexOutOfRange() {
-        PostalCode code = new PostalCode("K1Z-4J2");
-        assertFalse(isValidPostalCode(code));
+        try {
+            PostalCode code = new PostalCode("K1Z-4J2");
+            assertFalse(code.isValidPostalCode(code.getPostalCode()));
+        } catch (InvalidPostalCodeException e) {
+            //
+        }
     }
 
     @Test
     public void InvalidPostalCode_VindexNotChar() {
-        PostalCode code = new PostalCode("K11-4J2");
-        assertFalse(isValidPostalCode(code));
+        try {
+            PostalCode code = new PostalCode("K11-4J2");
+            assertFalse(code.isValidPostalCode(code.getPostalCode()));
+        } catch (InvalidPostalCodeException e) {
+            //
+        }
     }
 
     @Test
     public void InvalidPostalCode_HindexNotInt() {
-        PostalCode code = new PostalCode("K1B-CJ2");
-        assertFalse(isValidPostalCode(code));
+        try {
+            PostalCode code = new PostalCode("K1B-CJ2");
+            assertFalse(code.isValidPostalCode(code.getPostalCode()));
+        } catch (InvalidPostalCodeException e) {
+            //
+        }
     }
 
     @Test
     public void InvalidPostalCode_NoDash() {
-        PostalCode code = new PostalCode("K1B4J2");
-        assertFalse(isValidPostalCode(code));
+        try {
+            PostalCode code = new PostalCode("K1B4J2");
+            assertFalse(code.isValidPostalCode(code.getPostalCode()));
+        } catch (InvalidPostalCodeException e) {
+            //
+        }
     }
 
     @Test
     public void InvalidPostalCode_XisNotUpper() {
-        PostalCode code = new PostalCode("K1B-4j2");
-        assertFalse(isValidPostalCode(code));
+        try {
+            PostalCode code = new PostalCode("K1B-4j2");
+            assertFalse(code.isValidPostalCode(code.getPostalCode()));
+        } catch (InvalidPostalCodeException e) {
+            //
+        }
     }
 
     @Test
     public void InvalidPostalCode_XisNotLetter() {
-        PostalCode code = new PostalCode("K1B-432");
-        assertFalse(isValidPostalCode(code));
+        try {
+            PostalCode code = new PostalCode("K1B-432");
+            assertFalse(code.isValidPostalCode(code.getPostalCode()));
+        } catch (InvalidPostalCodeException e) {
+            //
+        }
     }
 
     @Test
     public void InvalidPostalCode_YisNotInt() {
-        PostalCode code = new PostalCode("K1B-4JX");
-        assertFalse(isValidPostalCode(code));
+        try {
+            PostalCode code = new PostalCode("K1B-4JX");
+            assertFalse(code.isValidPostalCode(code.getPostalCode()));
+        } catch (InvalidPostalCodeException e) {
+            // invalid throw
+        }
     }
 
     @Test
     public void ConstructorThrowsException() {
-        
+
     }
 
     @Test
     public void ConstructorWorks() {
-        PostalCode code = new PostalCode("K1B-4J2");
-
-        assertEquals(code.getRegionHorizontalIndex(), 4);
-        assertEquals(code.getRegionVerticalIndex(), 2);
-        assertEquals(code.getPostalCode(), "K1B-4J2");
+        try {
+            PostalCode code = new PostalCode("K1B-4J2");
+            assertEquals(code.getRegionHorizontalIndex(), 4);
+            assertEquals(code.getRegionVerticalIndex(), 2);
+            assertEquals(code.getPostalCode(), "K1B-4J2");
+        } catch (InvalidPostalCodeException e) {
+            //
+        }
     }
 }
