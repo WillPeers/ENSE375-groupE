933d5317300b0501074dc5eeff996cbbd612a3cd
Dhruv Modi <dhruvkmodi@gmail.com>
2021-03-13 19:54:15 -0700

    Patient Test Completed

--- RiskMeter/src/test/java/com/uregina/app/PatientTest.java
+++ RiskMeter/src/test/java/com/uregina/app/PatientTest.java
@@ -2,6 +2,11 @@ package com.uregina.app;
 
 import static org.junit.Assert.assertTrue;
 
+import com.uregina.exception.InvalidAgeException;
+import com.uregina.exception.InvalidIDException;
+import com.uregina.exception.InvalidNameException;
+import com.uregina.exception.InvalidPostalCodeException;
+
 import org.junit.Test;
 
 /**
@@ -18,28 +23,79 @@ public class PatientTest
         assertTrue(true);
     }
 
+  
     @Test
-    public void Validate_Alphabetic_True()
+    public void Validate_Alphabetic_True() throws InvalidNameException, InvalidAgeException, InvalidIDException, InvalidPostalCodeException
     {
+     String Name = "John";
+     String ID = null;
+     int AGE = 0;
+     PostalCode POSTALCODE = null;
+     Patient name = new Patient(Name, ID, AGE, POSTALCODE);
+     if (name.getName().matches("^[a-zA-Z]*$"))
+     {
+         assertTrue(true);
+     }
+     else
+     {
+         assertTrue(false);
+     }
+    }
 
+
+    @Test
+    public void Validate_AlphabeticDots_True() throws InvalidNameException, InvalidAgeException, InvalidIDException, InvalidPostalCodeException
+    {
+        String Name = "John.";
+        String ID = null;
+        int AGE = 0;
+        PostalCode POSTALCODE = null;
+        Patient name = new Patient(Name, ID, AGE, POSTALCODE);
+        if (name.getName().matches("^[a-zA-Z.]*$"))
+        {
+            assertTrue(true);
+        }
+        else
+        {
+            assertTrue(false);
+        } 
 
     }
 
     @Test
-    public void Validate_AlphabeticDots_True()
+    public void Validate_AlphabeticSpace_True() throws InvalidNameException, InvalidAgeException, InvalidIDException, InvalidPostalCodeException
     {
-
+        String Name = "John John";
+        String ID = null;
+        int AGE = 0;
+        PostalCode POSTALCODE = null;
+        Patient name = new Patient(Name, ID, AGE, POSTALCODE);
+        if (name.getName().matches("^[a-zA-Z][a-zA-Z ]+"))
+        {
+            assertTrue(true);
+        }
+        else
+        {
+            assertTrue(false);
+        }
     }
 
     @Test
-    public void Validate_AlphabeticSpace_True()
+    public void Validate_AlphabeticDotsSpace_True() throws InvalidNameException, InvalidAgeException, InvalidIDException, InvalidPostalCodeException
     {
-
-    }
-
-    @Test
-    public void Validate_AlphabeticDotsSpace_True()
-    {
+        String Name = "John. John";
+        String ID = null;
+        int AGE = 0;
+        PostalCode POSTALCODE = null;
+        Patient name = new Patient(Name, ID, AGE, POSTALCODE);
+        if (name.getName().matches("^[a-zA-Z][a-zA-Z .]+"))
+        {
+            assertTrue(true);
+        }
+        else
+        {
+            assertTrue(false);
+        }
         
-    }
+    } 
 }
